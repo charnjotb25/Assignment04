35a0abd62468ec9050ede4b69b80485a6062d1c6
charnjotb25 <71240488+charnjotb25@users.noreply.github.com>
2021-12-14 18:42:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 var updateView = async (button)=>{
       if (button.dataset.querytype =='by_title'){
         let queryvalue = document.querySelector('#titleQuery').value;
-        api = `http://localhost:3000/api/by_title/${queryvalue}`;
+          api = `https://charnjotb-assignment04.herokuapp.com/api/by_title/${queryvalue}`;
+       /* api = `http://localhost:3000/api/by_title/${queryvalue}`; */
 
     }else if (button.dataset.querytype =='by_course_code'){
         let queryvalue = document.querySelector('#courseNumberQuery').value;
-        api = `http://localhost:3000/api/by_course_code/${queryvalue}`;
+          api = `https://charnjotb-assignment04.herokuapp.com/api/by_course_code/${queryvalue}`;
+        /* api = `http://localhost:3000/api/by_course_code/${queryvalue}`; */
 
     }else if (button.dataset.querytype =='by_instructor'){
         let queryvalue = document.querySelector('#instructorQuery').value;
-        api = `http://localhost:3000/api/by_instructor/${queryvalue}`;
+          api = `https://charnjotb-assignment04.herokuapp.com/api/by_instructor/${queryvalue}`;
+       /* api = `http://localhost:3000/api/by_instructor/${queryvalue}`; */
 
     }else if (button.dataset.querytype =='by_level'){
         let queryvalue = document.querySelector('#levelQuery').value;
-        api = `http://localhost:3000/api/by_level/${queryvalue}`;
+         api = `https://charnjotb-assignment04.herokuapp.com/api/by_level/${queryvalue}`;    
+        /*  api = `http://localhost:3000/api/by_level/${queryvalue}`; */
 
     }else if (button.dataset.querytype =='combined_query'){
         let queryName = document.querySelector('#combined_instructorQuery').value;
         let queryLevel = document.querySelector('#combined_levelQuery').value;
-        api = `http://localhost:3000/api/combined_query/${queryName}/${queryLevel}`;
+        api = `https://charnjotb-assignment04.herokuapp.com/api/combined_query/${queryName}/${queryLevel}`;   
+        /* api = `http://localhost:3000/api/combined_query/${queryName}/${queryLevel}`; */
 
     }
     const data = await fetch(api);
